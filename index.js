f67c2caf336a14ed8757ca168d4836d9d3d2d249
Jesse <jessejamesjj007@gmail.com>
2025-06-16 13:28:59 +0300

    Add functional buttons

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 document.addEventListener ('DOMContentLoaded', () => {
     document.querySelector('form') .addEventListener('submit', (event) => {
         event.preventDefault()
-        guestNameShower(event.target.input.value)
+        const input = document.querySelector("#input");
+        guestNameShower(input.value);
+        input.value = "";
     })
 })
 function guestNameShower(guestName) {
     let list = document.querySelector("#list");
 
     let guestList = document.createElement("li");
-    guestList.textContent = `${guestName} `;
+
+    let nameSpan = document.createElement("span")
+    nameSpan.textContent = guestName;
+
+    let rsvpButton = document.createElement("button")
+    rsvpButton.textContent = "RSPV:"
+    rsvpButton.style.height = "48px"
+    rsvpButton.type = "button"
+
+
+  rsvpButton.addEventListener("click", () => {
+    if (rsvpButton.textContent.includes("Not Attending")) {
+      rsvpButton.textContent = "RSVP: Attending";
+      rsvpButton.style.backgroundColor = "lightgreen";
+      rsvpButton.style.marginTop = "auto"
+    } else {
+      rsvpButton.textContent = "RSVP: Not Attending";
+      rsvpButton.style.backgroundColor = "lightcoral";
+      rsvpButton.style.marginTop = "auto"
+    }
+  });
     
     let deleteButton = document.createElement("button");
     deleteButton.textContent = 'Remove';
-    deleteButton.addEventListener('click',nameDeleter(guestList))
+    deleteButton.addEventListener('click',nameDeleter)
+    deleteButton.type ="button"
    
-    guestList.appendChild(deleteButton);
-
     list.appendChild(guestList);
+    guestList.appendChild(nameSpan);
+    guestList.appendChild(deleteButton);
+    guestList.appendChild(rsvpButton)
 }
 
-function nameDeleter(name){
-    name.remove()
+function nameDeleter(event){
+    event.target.parentElement.remove()
 }
 
-function listLimiter(guestList){
-    
-}
+
