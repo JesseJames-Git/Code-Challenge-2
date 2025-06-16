1dd93d6aa486408474cdb360f21b4c845d97e842
Jesse <jessejamesjj007@gmail.com>
2025-06-16 15:06:01 +0300

    Add Stretch Delierables

--- index.js
+++ index.js
@@ -9,42 +9,99 @@ document.addEventListener ('DOMContentLoaded', () => {
 function guestNameShower(guestName) {
     let list = document.querySelector("#list");
 
+    if (list.childElementCount > 9) {
+        alert("Guest list is full")    
+        return
+    }
+
     let guestList = document.createElement("li");
 
     let nameSpan = document.createElement("span")
     nameSpan.textContent = guestName;
 
+
     let rsvpButton = document.createElement("button")
-    rsvpButton.textContent = "RSPV:"
-    rsvpButton.style.height = "48px"
+    rsvpButton.textContent = "Not Attending"
     rsvpButton.type = "button"
 
 
   rsvpButton.addEventListener("click", () => {
-    if (rsvpButton.textContent.includes("Not Attending")) {
-      rsvpButton.textContent = "RSVP: Attending";
+    if (rsvpButton.textContent === "Not Attending") {
+      rsvpButton.textContent = "Attending";
       rsvpButton.style.backgroundColor = "lightgreen";
-      rsvpButton.style.marginTop = "auto"
-    } else {
-      rsvpButton.textContent = "RSVP: Not Attending";
+      rsvpButton.style.marginTop = "10px"
+    } else{
+      rsvpButton.textContent = "Not Attending";
       rsvpButton.style.backgroundColor = "lightcoral";
-      rsvpButton.style.marginTop = "auto"
+      rsvpButton.style.marginTop = "10px"
     }
-  });
+})
+    let guestCategory = document.createElement("button")
+    guestCategory.textContent = "Friend"
+    guestCategory.type ="button"
+    
+    guestCategory.addEventListener("click", ()=> { 
+        if(guestCategory.textContent === "Friend"){
+            guestCategory.textContent = "Family"
+            guestCategory.style.backgroundColor = "Orange"
+        } else if (guestCategory.textContent === "Family") {
+            guestCategory.textContent = "Colleague"
+            guestCategory.style.backgroundColor = "Green"        
+        } else{
+            guestCategory.textContent = "Friend"
+            guestCategory.style.backgroundColor = "Yellow"
+        }   
+    });
     
     let deleteButton = document.createElement("button");
     deleteButton.textContent = 'Remove';
     deleteButton.addEventListener('click',nameDeleter)
     deleteButton.type ="button"
+
+    function nameDeleter(event){
+    event.target.parentElement.remove()
+}
+
+    let timeStamp = document.createElement("small")
+    let now = new Date()
+    timeStamp.textContent = `Added at: ${now.toLocaleTimeString()}`
+    timeStamp.style.marginLeft = "10px"
+    timeStamp.style.color = "bisque"
+
+
+    let editButton = document.createElement("button")
+    editButton.textContent = "Edit"
+    editButton.type ="button"
+
+    editButton.addEventListener("click" ,() => {      
+        let input = document.createElement("input")
+        input.type = "text"
+        input.value = nameSpan.textContent
+        
+        let saveButton = document.createElement("button")
+        saveButton.textContent = "Save"
+
+        guestList.replaceChild(input, nameSpan)
+        guestList.replaceChild(saveButton, editButton)
+
+        saveButton.addEventListener("click", () =>{
+            nameSpan.textContent = input.value
+            guestList.replaceChild(nameSpan ,input)
+            guestList.replaceChild(editButton ,saveButton)
+        })
+    })
+
+
    
     list.appendChild(guestList);
     guestList.appendChild(nameSpan);
+    guestList.appendChild(editButton)
+    guestList.appendChild(guestCategory)
     guestList.appendChild(deleteButton);
     guestList.appendChild(rsvpButton)
+    guestList.appendChild(timeStamp)
+    
 }
 
-function nameDeleter(event){
-    event.target.parentElement.remove()
-}
 
 
